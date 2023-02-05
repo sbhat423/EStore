e374042a57c7a1b7144e397af5195833edc1fff0
sharat bhat <sharattbhat@gmail.com>
2023-02-05 11:26:27 +1100

    Add EStore DB

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(option => 
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
---
+++ DataAccess/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace DataAccess
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Prodcts { get; set; }
+    }
+}
